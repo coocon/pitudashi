1cc494d13c90c0db3f4c3d20fd35c89993765c41
耿朋 <gengpeng@baijiahulian.com>
2021-03-15 21:20:03 +0800

    feat: init

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import 'bootstrap/js/dist/util';
+import 'bootstrap/js/dist/dropdown';
+import './scss/app.scss';
+
+import { Tooltip, Toast, Popover } from 'bootstrap';
+
+console.log('hi')
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+
+            {
+                test: /\.(scss)$/,
+                    use: [{
+                        // inject CSS to page
+                        loader: 'style-loader'
+                    }, {
+                        // translates CSS into CommonJS modules
+                        loader: 'css-loader'
+                    }, {
+                        // Run postcss actions
+                        loader: 'postcss-loader',
+                        options: {
+                            // `postcssOptions` is needed for postcss 8.x;
+                            // if you use postcss 7.x skip the key
+                            postcssOptions: {
+                                // postcss plugins, can be exported to postcss.config.js
+                                plugins: function () {
+                                    return [
+                                        require('autoprefixer')
+                                    ];
+                                }
+                            }
+                        }
+                    }, {
+                        // compiles Sass to CSS
+                        loader: 'sass-loader'
+                    }]
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        inject: "body",
+        template: "public/index.html",
+        filename: "index.html"
+    })],
+    devServer: {
+        contentBase: [path.join(__dirname, 'public'), path.join(__dirname, 'dist')],
+        compress: true,
+        port: 9000,
+        host: '0.0.0.0',
+        filename: 'bundle.js',
+        liveReload: false,
+        hot: true
+    },
+};
